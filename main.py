80cc8c50991c9283bb6f2cbe1e477d26a22478b7
Anastasia030 <a.nizovtseva@g.nsu.ru>
2023-10-29 21:29:54 +0700

    function draw hexagon

--- main.py
+++ main.py
@@ -31,10 +31,38 @@ def get_color_choice():
 
 
 def draw_hexagon(x, y, side_len, color):
-    pass
+    t.pencolor(color)
+    t.fillcolor(color)
+    t.pu()
+    t.goto(x, y)
+    t.pd()
+    t.begin_fill()
+    t.setheading(30)
+    for i in range(6):
+        t.forward(side_len)
+        t.left(300)
+    t.end_fill()
+    t.setheading(0)
 
 
 color_names = get_color_choice()
 quantity = get_num_hexagons()
 
-draw_hexagon()
+t.speed(5000)
+d = 500 // quantity
+side_len = d // (3 ** 0.5)
+number_rows = int(500 // (2 * side_len))
+
+for number in range(number_rows+1):
+    y_1 = 250 - 0.5 * side_len - (1.5 * side_len) * number
+    for hexagon in range(quantity+1):
+        if number == 0:
+            x_1 = -250 + (3 ** 0.5) * side_len * 0.5 + (3 ** 0.5) * side_len * hexagon
+            draw_hexagon(x_1, y_1, side_len, 'green')
+        elif number % 2 == 0:
+            x_1 = -250 + (3 ** 0.5) * side_len * 0.5 + (3 ** 0.5) * side_len * hexagon
+            draw_hexagon(x_1, y_1, side_len, 'green')
+        else:
+            x_1 = -250 + (3 ** 0.5) * side_len * hexagon
+            draw_hexagon(x_1, y_1, side_len, 'green')
+t.mainloop()
